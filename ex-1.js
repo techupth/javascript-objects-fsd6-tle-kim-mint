bcd8b08a0439497170f8bde2af14f3f027e3c925
PNKim <paljapon.nait@gmail.com>
2024-05-10 13:03:32 +0700

    feat: resolve ex1-3

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,14 @@
 // Exercise #1: Food Order
 
 // Start coding here
+const foodOrder = {
+  orderNumber: "A0234",
+  address: "Bangkok",
+  paymentTpye: "Cash",
+  totalPrice: 4500,
+  restaurantName: "MK",
+};
+
+foodOrder.totalPrice = 5000;
+foodOrder.paymentTpye = "Credit Card";
+console.log(foodOrder);
