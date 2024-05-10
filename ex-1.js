1e5227a93919dc4bc4e59d3aff46fa4b09104e12
tle <tlemakuz@gmail.com>
2024-05-10 13:17:02 +0700

    Fix:object debug

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,11 @@
-// Exercise #1: Food Order
+const foodOrder = {
+    orderNumber: "A0234",
+    address: "Bangkok",
+    paymentType: "Cash",
+    totaPrice: 4500,
+    restaurantName: "MK"
+};
+foodOrder.totaPrice = 5000,
+foodOrder.paymentType = "Credit Card"
+console.log(foodOrder);
 
-// Start coding here
